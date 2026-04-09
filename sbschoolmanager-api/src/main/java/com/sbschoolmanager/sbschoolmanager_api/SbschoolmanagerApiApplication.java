31013ab5f95069872ae2f6b5f72b04ee2c3d1234
jesus <jesusregueiravazquez@gmail.com>
2026-04-09 20:23:19 +0200

    Creación del repositorio, para el proyecto SbSchoolManager, la estructura actual fue creada con spring initialzr

---
+++ sbschoolmanager-api/src/main/java/com/sbschoolmanager/sbschoolmanager_api/SbschoolmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.sbschoolmanager.sbschoolmanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbschoolmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbschoolmanagerApiApplication.class, args);
+	}
+
+}
---
+++ sbschoolmanager-api/src/test/java/com/sbschoolmanager/sbschoolmanager_api/SbschoolmanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbschoolmanager.sbschoolmanager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbschoolmanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
