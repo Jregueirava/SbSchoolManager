2868715200425e1dc79fe6f45ebef2c7a4ef6edb
jesus <jesusregueiravazquez@gmail.com>
2026-09-15 11:35:56 +0200

    service horario listo

--- sbschoolmanager_app/lib/services/horario_service.dart
+++ sbschoolmanager_app/lib/services/horario_service.dart
@@ -8,5 +8,30 @@ class HorarioService {
     final response = await http.get(
       Uri.parse("${ApiConfig.horario}/clase/$codClase"),
     );
+    if (response statusCode == 200){
+      final List<dynamic> jsonList = jsonDecode(response.body);
+      return jsonList.map((json)=> Horario.fromJson(json)).toList(); 
+    }
+    throw Exception("Error al cargar horarios: ${response.statusCode}");
+  }
+
+  Future<Horario> crear (Horario horario) async{
+    final response = await http.post(
+      Uri.parse(ApiConfig.horarios),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(horario.toJson()),
+    );
+    if(response.statusCode == 200){
+      return Horario.fromJson(jsonDecode(respnse.body));
+    }
+    throw Exception("Error al crear horario: ${response.statusCode}");
+  }
+
+  Future<void> eliminar(int id) async{
+    final response =
+      await http.delete(Uri.parse("${ApiConfig.horarios}/$id"));
+    if(respnse.statusCode !=204){
+      throw Exception("Error al eliminar horario: ${response.statusCode}");
+    }  
   }
 }
