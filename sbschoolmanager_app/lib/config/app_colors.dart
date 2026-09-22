d2222c9c3e4eace4a53d5f7638aca00a11c3f0d1
jesus <jesusregueiravazquez@gmail.com>
2026-09-22 11:49:53 +0200

    paleta de colores de la app

---
+++ sbschoolmanager_app/lib/config/app_colors.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+
+class AppColors{
+  //Primarios 
+  static const primary = Color(0xFF1A237E); //Azul oscuro
+  static const primaryLight = Color(0xFF3949AB); //Azul medio
+  static const primarySoft = Color(0xFFE8EAF6); //Azul suave
+
+  //Grises
+
+  static const grisOscuro = Color(0xFF37474F); //Gris azulado oscuro
+  static const grisMedio = Color(0xFF78909C); //Gris azulado medio
+  static const grisClaro = Color(0xFFB0BEC5); //Gris claro
+  static const grisSuave = Color(0xFFECEFF1); //Gris muy suave
+  static const grisFondo = Color(0xFFF5F6FA); //Fondo
+
+  //Blancos
+  static const blanco = Color(0xFFFFFFFF); 
+  static const blancoSuave = Color(0xFFFAFAFA);
+
+  //Colores para badges y estados
+  static const exito = Color(0xFF43A047); //Verde
+  static const exitoSuave = Color(0xFFE8F5E9);
+  static const advertencia = Color(0xFFFFB300); //ambar
+  static const advertenciaSuave = Color (0xFFFFF8E1);
+  static const error = Color(0xFFE53935); //rojo
+  static const errorSuave = Color(0xFFFFEBEE);
+
+  //Colores niveles de grupo
+
+  static const principiante = Color(0xFF1565C0);
+  static const principianteSuave = Color(0xFFE3F2FD);
+  static const intermedio = Color(0xFF37474F);
+  static const intermedioSuave = Color(0xFFECEFF1);
+  static const avanzado = Color(0xFF283593);
+  static const avanzadoSuave = Color(0xFFE8EAF6);
+}
